188bb62e5f705395f1bc4fc5999d7c46a63f46f3
Andreas <andy.reitberger@gmail.com>
2023-08-06 08:02:32 +0200

    Added source code

---
+++ src/VisualStateIssueMuai/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VisualStateIssueMuai
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/VisualStateIssueMuai/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VisualStateIssueMuai
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
